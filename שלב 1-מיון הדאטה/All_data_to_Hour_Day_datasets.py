2ea590bf1fddbedc859ad25e9b2f0bc6c53e6562
yuvalgeva77 <yuvalgeva77@gmail.com>
2020-06-24 00:27:59 +0300

    start statistics
    add mote than 10 and domain

--- שלב 1-מיון הדאטה/All_data_to_Hour_Day_datasets.py
+++ שלב 1-מיון הדאטה/All_data_to_Hour_Day_datasets.py
@@ -2,8 +2,7 @@ import pandas as pd
 import os
 import matplotlib.dates as md
 import matplotlib.pyplot as plt
-import gc
-import json
+import pickle
 #Gets "data.csv" - all data given to us
 #filters it by day and by hour
 # creates 'daySet.csv' 'hourSet.csv'
@@ -20,14 +19,14 @@ def create_folder(dirName):
     print(path)
     return path
 
-allData = pd.read_csv("allData.csv")[
-    ["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size"]]  # keep relavent coulmns #dataframe
 
-files = [[]]
+
+
 def create_dataset():
-    # allData = pd.read_csv("data.csv")[
-     #   ["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID"]]  # keep relavent coulmns #dataframe
-
+    allData = pd.read_csv("data.csv")[
+        ["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size",
+         "WebFileUrlDomain"]]  # keep relavent coulmns #dataframe
+    files = [[]]
     # --------------------create a dataSet to each file------------------------------
     allData.sort_values(by=['Sha1ID', 'ReportTime'], ascending=[True, True], inplace=True)
     i = -1
@@ -38,20 +37,20 @@ def create_dataset():
     for sha in allData["Sha1ID"]:
         if i%5000 == 0: #TODO check
             print(i)
+        if sha==15201105:
+            print("A")
         i = i + 1
         if currF == sha:
             files[j].append(
-                [allData.iloc[i][0], allData.iloc[i][1], allData.iloc[i][2], allData.iloc[i][3], allData.iloc[i][4]]
-            )
+                [allData.iloc[i][0], allData.iloc[i][1], allData.iloc[i][2], allData.iloc[i][3], allData.iloc[i][4], allData.iloc[i][5]])
         else:
-            files[j] = pd.DataFrame(files[j], columns=["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size"])
+            files[j] = pd.DataFrame(files[j], columns=["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size","WebFileUrlDomain"])
             currF = sha
             j = j + 1
             files.append([])
             files[j].append(
-                [allData.iloc[i][0], allData.iloc[i][1], allData.iloc[i][2], allData.iloc[i][3], allData.iloc[i][4]]
-            )
-    files[j] = pd.DataFrame(files[j], columns=["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size"])
+                [allData.iloc[i][0], allData.iloc[i][1], allData.iloc[i][2], allData.iloc[i][3], allData.iloc[i][4],allData.iloc[i][5]])
+    files[j] = pd.DataFrame(files[j], columns=["Sha1ID", "ThreatNameID", "ReportTime", "MachineGuidID", "Size","WebFileUrlDomain"])
     return files
 
     #del allData
@@ -66,6 +65,8 @@ def sortByMachines(files):
     for i in range(len(files)):
         if i%5000 == 0: #TODO check
             print(i)
+        if i == 423:
+            print("A")
         # add label malicious
         if ((files[i].ThreatNameID.values != 5644).sum() > 0):
             files[i]['Malicious'] = True
@@ -77,39 +78,39 @@ def sortByMachines(files):
         # -------------------create time set to each file dataset - machine vs hours-----------------
         files[i]['ReportTime'] = pd.to_datetime(files[i]['ReportTime'])
         # ------------hourSet - hour vs machine----------------
-        hourSet.append(files[i].groupby([pd.Grouper(key='ReportTime', freq='H'), "Sha1ID", "Malicious", "Size"]).size().reset_index(
+        hourSet.append(files[i].groupby([pd.Grouper(key='ReportTime', freq='H'), "Sha1ID", "Malicious", "Size","WebFileUrlDomain"]).size().reset_index(
                 name='HourlyMachineCount'))
         # Clean-Prevalent file (more than X machines)
-        if (hourSet[i]['HourlyMachineCount'].sum() > 100):
-            hourSet[i]['MoreThan100'] = True
+        if (hourSet[i]['HourlyMachineCount'].sum() > 10):
+            hourSet[i]['MoreThan10'] = True
         else:
-            hourSet[i]['MoreThan100'] = False
+            hourSet[i]['MoreThan10'] = False
 
 
         # ------------ daySet-day vs machine-----------------
-        daySet.append(files[i].groupby([pd.Grouper(key='ReportTime', freq='D'), "Sha1ID", "Malicious", "Size"]).size().reset_index(
+        daySet.append(files[i].groupby([pd.Grouper(key='ReportTime', freq='D'), "Sha1ID", "Malicious", "Size","WebFileUrlDomain"]).size().reset_index(
             name='DailyMachineCount'))
 
         # Clean-Prevalent file (more than X machines)
-        if (daySet[i]['DailyMachineCount'].sum() > 100):
-            daySet[i]['MoreThan100'] = True
+        if (daySet[i]['DailyMachineCount'].sum() > 10):
+            daySet[i]['MoreThan10'] = True
         else:
-            daySet[i]['MoreThan100'] = False
+            daySet[i]['MoreThan10'] = False
 
     return (daySet,hourSet,len(files))
 
  #-------------got from pre function ----------
-import pickle
+
 
 files = create_dataset()
-with open('FILES.pkl', 'wb') as f:
-    pickle.dump(files, f)
+# with open('FILES.pkl', 'wb') as f:
+#     pickle.dump(files, f)
 
-# (daySet,hourSet,numfiles)=sortByMachines(create_dataset())
-# print("dataset created")
-#
-# pd.concat(daySet).to_csv('daySet.csv')
-# pd.concat(hourSet).to_csv('hourSet.csv')
+(daySet,hourSet,numfiles)=sortByMachines(create_dataset())
+print("dataset created")
+
+pd.concat(daySet).to_csv('daySet.csv')
+pd.concat(hourSet).to_csv('hourSet.csv')
 
 print("saved")
 
--- שלב 2-מרחקים/features.py
+++ שלב 2-מרחקים/features.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import os
 import  numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
 
+#-------------Get the data-----------
 malFile = pd.read_csv('malicious files day data.csv')[["Sha1ID","Malicious", "mean", "std", "size"]]
 cleanFile = pd.read_csv('clean files day data.csv')[["Sha1ID","Malicious", "mean", "std", "size"]]
 
@@ -15,7 +19,7 @@ features = pd.concat([features, distances],axis=1,ignore_index=True)
 
 features.columns = ["Sha1ID","Malicious","Day count Mean","Day count STD","Size","DTW k-mean","DTW k-median","DTW k-STD","DTW k-Malicious(%)",
                     "Euclidean k-mean","Euclidean k-median","Euclidean k-STD","Euclidean k-Malicious(%)"]
-#----max--
+#----max,min--
 malFile_array = pd.read_csv('malicious files day data.csv')[["day_Array"]]
 cleanFile_array  = pd.read_csv('clean files day data.csv')[["day_Array"]]
 arrays = pd.concat([malFile_array, cleanFile_array], axis=0, ignore_index=True)
@@ -25,14 +29,44 @@ for i in range(len(arrays)):
     a = arrays["day_Array"][i]
     a = map(int, list(a[1:-1].split()))
     a = np.array([int(s) for s in a])
-    features.at[i, 'max Day count'] = max(a)
-    features.at[i, 'min Day count'] = min(a)
-    # features.ix[["max", i]] =max(a)
-    # features.ix[["max", i]]=min(a)
+    features.at[i, 'max Day count'] = float(max(a))
+    features.at[i, 'min Day count'] = float(min(a))
 
-
-
-
-
+#create features.csv
 parent_dir = os.getcwd()
-features.to_csv(os.path.join(parent_dir, "features.csv"))
+# features.to_csv(os.path.join(parent_dir, "features.csv"))
+
+#------------------------Data exploration---------------------------------
+
+# sns.countplot(x='Malicious',data=features)
+# plt.show()
+for title in list(features.columns) :
+    # plt.title("file {0}: Machine per Day".format(fileSha))
+    if(title!="Sha1ID"):
+        # sns.set(style="darkgrid")
+        # plt.figure(figsize=(12, 8))
+        # ax=sns.countplot(x=title,hue='Malicious',data=features)
+        # # ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right",fontsize=7)
+        # # plt.tight_layout()
+        if (title != "Malicious" and title!='max Day count' and title!='min Day count'):
+             # features.hist(column=title, bins='auto',figsize=(12, 8),xrot=14)
+             g = sns.catplot(x=title, hue="Malicious", col="Malicious",
+                             data=features, kind="count",
+                             )
+             g.set(ylim=(0, max(features[title].tolist())+1))
+
+        #     # fig = plt.figure()
+        #     x=features[title].tolist()
+        #     plt.hist(x, bins=10)
+        #     plt.xticks(rotation=35)
+        #     plt.tick_params(axis='x', which='major', labelsize=7)
+        #     plt.tight_layout()
+        # ax.set_ylim(auto=True)
+        #features.hist(column=title, bins=50)
+        # plt.tight_layout()
+        path = os.path.join(parent_dir, "{0} Distribution.png".format(title))
+        plt.savefig(path)
+        # plt.show()
+        plt.clf()
+        plt.close()
+        print("{0}\n".format(title))
